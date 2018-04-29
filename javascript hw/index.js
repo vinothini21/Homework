4c319af423c65f9175a2e1473f503981ba163c93
Vinu <vinothinichem2015@gmail.com>
2018-04-29 00:10:00 -0700

    modified file
    edited codes and its working

--- javascript hw/index.js
+++ javascript hw/index.js
@@ -1,10 +1,15 @@
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
 var $datetimeInput = document.querySelector("#datetime");
+var $cityInput = document.querySelector("#city");
+var $stateInput = document.querySelector("#state");
+var $countryInput = document.querySelector("#country");
+var $shapeInput = document.querySelector("#shape");
 var $searchBtn = document.querySelector("#search");
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
+
 
 // Set filtereddata to dataSet initially
 var filteredData = dataSet;
@@ -26,17 +31,32 @@ function renderTable() {
     }
   }
 }
-
+// Filter by Date/time
 function handleSearchButtonClick() {
   // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterdatetime = $datetimeInput.value.trim().toLowerCase();
+  var filterDatetime = $datetimeInput.value.trim().toLowerCase();
+  var filterCity = $cityInput.value.trim().toLowerCase();
+  var filterState = $stateInput.value.trim().toLowerCase();
+  var filterCountry = $countryInput.value.trim().toLowerCase(); 
+  var filterShape = $shapeInput.value.trim().toLowerCase();
 
+  
   // Set filtereddata to an array of all data whose "datetime" matches the filter
   filteredData = dataSet.filter(function(data) {
     var dataDatetime = data.datetime.toLowerCase();
+    var dataCity = data.city.toLowerCase();
+    var dataState = data.state.toLowerCase();
+    var dataCountry = data.country.toLowerCase();
+    var dataShape = data.shape.toLowerCase();
 
-    // If true, add the datetime to the filtereddata, otherwise don't add it to filtereddata
-    return dataDatetime === filterdatetime;
+    // filtering the columns
+    var filteredFields =
+      (filterDatetime === "" || dataDatetime === filterDatetime) &&
+      (filterCity === "" || dataCity === filterCity) &&
+      (filterState === "" || dataState === filterState) &&
+      (filterCountry === "" || dataCountry === filterCountry) &&
+      (filterShape === "" || dataShape === filterShape);
+    return filteredFields;
   });
   renderTable();
 }
