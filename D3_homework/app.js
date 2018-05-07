8762dc36c6dbfa98921dc6deef43bb93f9f2c2ba
Vinu <vinothinichem2015@gmail.com>
2018-05-06 17:46:37 -0700

    modified
    added extra features for interactive graph

--- D3_homework/app.js
+++ D3_homework/app.js
@@ -13,7 +13,7 @@ var margin = {top: 20, right: 40, bottom: 60, left: 100},
 var xValue = function(d) { return d["Income_100,000_or_more"];}, // data -> value
     xScale = d3.scale.linear().range([0, width]), // value -> display
     xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(7);
 
 // setup y
 var yValue = function(d) { return d.College_or_more;}, // data -> value
@@ -22,7 +22,7 @@ var yValue = function(d) { return d.College_or_more;}, // data -> value
     yAxis = d3.svg.axis().scale(yScale).orient("left");
 
 // setup fill color
-var cValue = function(d) { return d.State;},
+var cValue = function(d) { return d.stateName;},
    color = d3.scale.category10();
 
 // add the graph canvas to the body of the webpage
@@ -47,12 +47,13 @@ d3.csv("data.csv", function(error, data) {
 //    console.log(d);
   });
 
-  // don't want dots overlapping axis, so add in buffer to data domain
+  // don't want circle overlapping axis, so add in buffer to data domain
   xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
   yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
  
   var currentAxisLabelX = "Income_100,000_or_more";
   var currentAxisLabelY = "College_or_more";
+
   // x-axis
   svg.append("g")
       .attr("class", "x axis")
@@ -62,6 +63,9 @@ d3.csv("data.csv", function(error, data) {
       .attr("class", "x label")
       .attr("x",  width/ 3)
       .attr("y",  0 - margin.right + 80)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "15px")
+      .attr("font-weight", "bold")
       .text("Income 100,000+");
       
   // y-axis
@@ -71,12 +75,15 @@ d3.csv("data.csv", function(error, data) {
     .append("text")
       .attr("class", "y label")
       .attr("transform", "rotate(-90)")
-      .attr("y",  0 - margin.left + 60)
+      .attr("y",  0 - margin.left + 50)
       .attr("dy", "1em")
-      .attr("x", 0 - height / 2)
+      .attr("x", 0 - height / 1.5)
+      .attr("font-weight", "bold")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "15px")
       .text("Atleast a degree");
 
-  // draw dots
+  // draw circle for the points
   svg.selectAll("circle")
       .data(data)
       .enter()
@@ -91,7 +98,7 @@ d3.csv("data.csv", function(error, data) {
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html(d["State"] + "<br/> (" + xValue(d) 
+          tooltip.html(d["stateName"] + "<br/> (" + xValue(d) 
 	        + ", " + yValue(d) + ")")
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
@@ -150,5 +157,5 @@ function resize() {
     width = width - margin.left - margin.right;
     x.range([0, width]);
 
-    // do the actual resize...
+    
 }
